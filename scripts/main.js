3ee4a8f40ffcced7c8a9b7fae7c3e8eb40dfa63d
Björn Blom <bjornblom@gmail.com>
2015-11-04 14:39:50 +0100

    added routing and helpers

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var helpers = require('./helpers');
+
 var App = React.createClass({
 
     render: function() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
                 <Inventory />
@@ -21,7 +29,15 @@ var Header = React.createClass({
 
     render: function() {
         return (
-            <p>Header</p>
+            <header className="top">
+                <h1>Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                Day</h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+            </header>
         )
     }
 });
@@ -50,7 +66,7 @@ var StorePicker = React.createClass({
         return (
             <form className="store-selector">
                 <h2>Please Enter A Store</h2>
-                <input type="text" ref="storeId" required />
+                <input type="text" defaultValue={helpers.getFunName()} ref="storeId" required />
                 <input type="Submit" />
             </form>
         )
@@ -58,4 +74,22 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+var NotFound = React.createClass({
+
+    render: function() {
+        return (
+            <h1>Not found</h1>
+        )
+    }
+
+})
+
+var routes = (
+    <Router history={createBrowserHistory()}>
+        <Route path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route path="*" component={NotFound} />
+    </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));
