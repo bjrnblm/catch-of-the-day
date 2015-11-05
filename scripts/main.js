c99dbec5dee4f7d0c73dd3d16fc3ece634fbf21d
Björn Blom <bjornblom@gmail.com>
2015-11-05 17:23:57 +0100

    order list added

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ var App = React.createClass({
     },
 
     addToOrder: function(key) {
-        this.state.order[key] = this.state.order['key'] +1 || 1;
+        this.state.order[key] = this.state.order[key] +1 || 1;
         this.setState({ order: this.state.order });
     },
 
@@ -52,7 +52,7 @@ var App = React.createClass({
                         {Object.keys(this.state.fishes).map(this.renderFish)}
                     </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
         )
@@ -144,9 +144,53 @@ var Header = React.createClass({
 
 var Order = React.createClass({
 
-    render: function() {
+    renderOrder: function(key) {
+        var fish = this.props.fishes[key];
+        var count = this.props.order[key];
+
+        if(!fish){
+            return (
+                <li key={key}>
+                    Sorry, Fish no longer available!
+                </li>
+            )
+        }
+
         return (
-            <p>Order</p>
+            <li key={key}>
+                {count}lbs
+                {fish.name}
+                <span className="price">{helpers.formatPrice(count * fish.price)}</span>
+            </li>
+        )
+    },
+
+    render: function() {
+        var orderIds = Object.keys(this.props.order);
+
+        var total = orderIds.reduce((prevTotal, key)=> {
+            var fish = this.props.fishes[key];
+            var count = this.props.order[key];
+            var isAvailable = fish && fish.status === 'available';
+
+            if(fish && isAvailable){
+                return prevTotal + (count * parseInt(fish.price) || 0);
+            }
+
+            return prevTotal
+        }, 0);
+
+        return (
+            <div className="order-wrap">
+                <h2 className="order-title">Your Order</h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                    <li className="total">
+                        <strong>Total:</strong>
+                        {helpers.formatPrice(total)}
+                    </li>
+                </ul>
+            </div>
         )
     }
 });
